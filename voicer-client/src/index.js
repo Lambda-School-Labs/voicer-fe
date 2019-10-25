af6bb0749b60802a7c5f467f256617e0b4e6a84b
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2019-10-24 23:43:26 -0400

    Added tab functionality to OnboardingView.js

--- voicer-client/src/index.js
+++ voicer-client/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 // import rootReducer from './reducers';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 const store = createStore(() => ({}), applyMiddleware(thunk, logger));
--- voicer-client/src/views/OnboardingView.js
+++ voicer-client/src/views/OnboardingView.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from '../components/Login/Login';
 import Register from '../components/Register';
+import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
+import classnames from 'classnames';
 
-const OnboardingView = (props) => {
+const OnboardingView = () => {
+    const [activeTab, setActiveTab] = useState('1');
+  
+    const toggle = tab => {
+      if(activeTab !== tab) setActiveTab(tab);
+    }
+
     return (
     <div>
-        <Login />
-        <Register />
+        <Nav tabs>
+            <NavItem>
+                <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => { toggle('1'); }}
+                >
+                    Login
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => { toggle('2'); }}
+                >
+                    Register
+                </NavLink>
+            </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+                <Login />
+            </TabPane>
+            <TabPane tabId="2">
+                <Register />
+            </TabPane>
+        </TabContent>
     </div>
     );
 }
