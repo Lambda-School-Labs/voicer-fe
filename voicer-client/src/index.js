8435fc613c4c340117f8c5fe24ab88090ab37683
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2019-10-24 23:11:07 -0400

    Commented out imports of not yet existent files

--- voicer-client/src/index.js
+++ voicer-client/src/index.js
@@ -6,9 +6,10 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+// import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+// const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(() => ({}), applyMiddleware(thunk, logger));
 
 ReactDOM.render(
     <Provider store={store} >
