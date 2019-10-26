32c5c0c0970f6cf91773a96a239df378bae663b3
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2019-10-26 13:42:36 -0400

    Edited the layout of OnboardingView.js

--- voicer-client/src/views/OnboardingView.js
+++ voicer-client/src/views/OnboardingView.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Login from '../components/Login/Login';
 import Register from '../components/Register';
-import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
+import { Container, Row, Col, Card, CardBody, TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import classnames from 'classnames';
 
 const OnboardingView = () => {
@@ -13,32 +13,42 @@ const OnboardingView = () => {
 
     return (
     <div>
-        <Nav tabs>
-            <NavItem>
-                <NavLink
-                className={classnames({ active: activeTab === '1' })}
-                onClick={() => { toggle('1'); }}
-                >
-                    Login
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink
-                className={classnames({ active: activeTab === '2' })}
-                onClick={() => { toggle('2'); }}
-                >
-                    Register
-                </NavLink>
-            </NavItem>
-        </Nav>
-        <TabContent activeTab={activeTab}>
-            <TabPane tabId="1">
-                <Login />
-            </TabPane>
-            <TabPane tabId="2">
-                <Register />
-            </TabPane>
-        </TabContent>
+        <Container>
+            <Row>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <Nav tabs>
+                                <NavItem>
+                                    <NavLink
+                                    className={classnames({ active: activeTab === '1' })}
+                                    onClick={() => { toggle('1'); }}
+                                    >
+                                        Login
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink
+                                    className={classnames({ active: activeTab === '2' })}
+                                    onClick={() => { toggle('2'); }}
+                                    >
+                                        Register
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <TabContent activeTab={activeTab}>
+                                <TabPane tabId="1">
+                                    <Login />
+                                </TabPane>
+                                <TabPane tabId="2">
+                                    <Register />
+                                </TabPane>
+                            </TabContent>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     </div>
     );
 }
